0a8124f1d6d39d09485c2e8e5e15074dd5f541e9
samhcheung <samhcheung@gmail.com>
2016-08-08 16:30:46 -0700

    Passing all tests but still need to implement all functions and modify html.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,72 @@
 // YOUR CODE HERE:
 
+var app = {};
+
+app.server = 'https://api.parse.com/1/classes/messages';
+
+app.init = function () {
+  $('.username').click(app.addFriend());
+  $('#send .submit').submit(app.handleSubmit());
+
+};
+
+app.send = function (message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+};
+
+app.fetch = function () {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+};
+
+app.clearMessages = function () {
+  $('#chats').html('');
+
+};
+
+app.addMessage = function (message) {
+  $('#chats').append('<div>' + message.roomname + ' ' + '<a class="username">' + message.username + '</a>' + ': ' + message.text + '</div>');
+  app.init();
+};
+
+app.addRoom = function (room) {
+  $('#roomSelect').append('<div id=' + room + '></div>');
+};
+
+app.addFriend = function() {
+  
+
+};
+
+app.handleSubmit = function() {
+
+
+};
+
+
+
+
